d3b337bd6ded03768920f3ed0b05cf212e3866db
tanjil <tanjil14@cse.pstu.ac.bd>
2020-10-22 19:26:37 +0600

    Updates

---
+++ precache-manifest.e6ec90b72a068607b521aaf93ed6bb7f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc8be6b6d6d4832f1239df43c3f983c6",
+    "url": "/smart-poll/index.html"
+  },
+  {
+    "revision": "191684054fc3f98a835e",
+    "url": "/smart-poll/static/css/2.6414bb7b.chunk.css"
+  },
+  {
+    "revision": "ff0cd7ae4ef25d08baa6",
+    "url": "/smart-poll/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "191684054fc3f98a835e",
+    "url": "/smart-poll/static/js/2.616c8f5a.chunk.js"
+  },
+  {
+    "revision": "8a137f586fcebb1a370343eb47b7677c",
+    "url": "/smart-poll/static/js/2.616c8f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0cd7ae4ef25d08baa6",
+    "url": "/smart-poll/static/js/main.45e0435c.chunk.js"
+  },
+  {
+    "revision": "e2ebb35afff04e1de8ff",
+    "url": "/smart-poll/static/js/runtime-main.5cf660a0.js"
+  }
+]);
